5b5e3e1d6cc9256ed3ce8bbe1f63fda0187ff09f
João Gerliandro <joaogerliandro@gmail.com>
2023-12-26 14:14:22 -0300

    feature - adding new ui format

--- source/mainwindow.cpp
+++ source/mainwindow.cpp
@@ -15,22 +15,22 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_start_button_clicked()
 {
-    if(!file_path.isEmpty())
+    /*if(!file_path.isEmpty())
     {
         QProcess::startDetached(file_path, QStringList(), directory_path);
 
         this->close();
-    }
+    }*/
 }
 
 void MainWindow::on_open_file_button_clicked()
 {
-    file_path = QFileDialog::getOpenFileName(this, "Open a file", ".", "Executable (*.exe)");
+    /*file_path = QFileDialog::getOpenFileName(this, "Open a file", ".", "Executable (*.exe)");
 
     QDir file_dir = QFileInfo(file_path).absoluteDir();
 
     directory_path = file_dir.absolutePath();
 
     if(!file_path.isEmpty())
-        ui->file_path->setText(file_path);
+        ui->file_path->setText(file_path);*/
 }
